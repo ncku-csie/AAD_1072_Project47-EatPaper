9f43885857b70cbe98b4694c3f1d96921a07727e
coliver0221 <coliver0221.cs03@nctu.edu.tw>
2019-06-17 19:25:31 +0800

    add filter & modify sorter

--- EatPaper/app/src/main/java/com/yoyohung/eatpaper/PaperListActivity.java
+++ EatPaper/app/src/main/java/com/yoyohung/eatpaper/PaperListActivity.java
@@ -14,8 +14,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.yoyohung.eatpaper.PaperDetailActivity;
-import com.yoyohung.eatpaper.R;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yoyohung.eatpaper.adapter.PaperListAdapter;
 import com.yoyohung.eatpaper.model.Paper;
 
@@ -34,6 +33,11 @@ public class PaperListActivity extends AppCompatActivity implements
     private Query mQuery;
     //private static final int LIMIT = 20;
 
+    // spinner
+    private Spinner sorter_spinner;
+    private String[] sorter_list;
+    private String sorter_type;
+
     private Spinner filter_spinner;
     private String[] filter_list;
     private String filter_type;
@@ -43,19 +47,40 @@ public class PaperListActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paper_list);
 
-        // init filter spinner
+        // init spinner variable
+        sorter_spinner = findViewById(R.id.spinner_sorter);
+        sorter_list = getResources().getStringArray(R.array.sorter_list);
+        sorter_type = sorter_list[0];
         filter_spinner = findViewById(R.id.spinner_filter);
         filter_list = getResources().getStringArray(R.array.filter_list);
-        filter_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        filter_type = filter_list[0];
+
+        // init filter spinner
+        sorter_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                filter_type = filter_list[position];
-                onFilter(filter_type);
+                sorter_type = sorter_list[position];
+                //onSorter(sorter_type);
+                onSorterFilter(sorter_type, filter_type);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                filter_type = filter_list[0]; // default : stock
+                sorter_type = sorter_list[0]; // default : stock
+            }
+        });
+
+        // init filter spinner
+        filter_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                filter_type = filter_list[position];
+                onSorterFilter(sorter_type, filter_type);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                filter_type = filter_list[0];
             }
         });
 
@@ -128,16 +153,16 @@ public class PaperListActivity extends AppCompatActivity implements
         startActivity(intent);
     }
 
-    public void onFilter(String filter_type){
+    public void onSorter(String sorter_type){
         // construct basic query
         Query query = mFirestore.collection("paperStock");
 
-        switch(filter_type){
+        switch(sorter_type){
             case "紙名" :
                 query = query.orderBy("paperName", Query.Direction.DESCENDING);
                 break;
             case "庫存量":
-                query = query.orderBy("currentQuantity", Query.Direction.DESCENDING);
+                query = query.orderBy("currentQuantity", Query.Direction.ASCENDING);
                 break;
             case "顏色":
                 query = query.orderBy("paperColor", Query.Direction.DESCENDING);
@@ -154,6 +179,60 @@ public class PaperListActivity extends AppCompatActivity implements
         mQuery = query;
         mAdapter.setQuery(query);
         //mAdapter.notifyDataSetChanged();
+    }
+
+    public void onSorterFilter(String sorter_type, String filter_type){
+        // construct basic query
+        Query query = mFirestore.collection("paperStock");
+
+        // filter
+        switch (filter_type){
+            case "所有紙種":
+                break;
+            case "封面紙":
+                query = query.whereEqualTo("paperType", "封面紙");
+                break;
+            case "色紙":
+                query = query.whereEqualTo("paperType", "色紙");
+                break;
+            case "影印紙":
+                query = query.whereEqualTo("paperType", "影印紙");
+                break;
+            case "貼紙":
+                query = query.whereEqualTo("paperType", "貼紙");
+                break;
+            case "上光膜":
+                query = query.whereEqualTo("paperType", "上光膜");
+                break;
+            case "大圖捲紙":
+                query = query.whereEqualTo("paperType", "大圖捲紙");
+                break;
+            default:
+                break;
+        }
+
+        // sorter
+        switch(sorter_type){
+            case "紙名" :
+                query = query.orderBy("paperName", Query.Direction.DESCENDING);
+                break;
+            case "庫存量":
+                query = query.orderBy("currentQuantity", Query.Direction.ASCENDING);
+                break;
+            case "顏色":
+                query = query.orderBy("paperColor", Query.Direction.DESCENDING);
+                break;
+            case "紙品種類":
+                query = query.orderBy("paperType", Query.Direction.DESCENDING);
+                break;
+            default:
+                query = query.orderBy("paperName", Query.Direction.DESCENDING);
+                break;
+        }
+
+        // update the query
+        mQuery = query;
+        mAdapter.setQuery(query);
     }
 
     @Override
